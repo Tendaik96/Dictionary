72a4d3fc81e6431f579fc32d257247b5bf51e953
Tendai Karuwo <tendaikaruwo96@gmail.com>
2023-05-16 16:08:22 +0100

    first MVP complete

--- main.js
+++ main.js
@@ -1,47 +1,63 @@
 // capture search box
-const inputSearch = document.querySelector("#searchBox");
+let inputSearch = document.querySelector("#searchBox");
 
 /* inputSearch.addEventListener("change", handleChange); */
 
 // capture button
 const button = document.querySelector("#button");
 
+// get all headings by id
+
+let meaning = document.getElementById('meaning')
+let noun = document.getElementById("noun");
+let verb = document.getElementById("verb");
+let adverb = document.getElementById("adverb");
+let adjective = document.getElementById("adjective");
+  
 //test if the button works
 /* button.addEventListener("click", () => {
    console.log("clicked")
 }); */
 
 //check input value in console - value prints in console
-button.addEventListener("click", () => {
-  console.log(inputSearch.value)
- // inputSearch.value="";
-});
 
 // get value from input and use it to search API
-button.addEventListener("click", getDefinition);
+// button.addEventListener("click", (inputSearch) => {
+//   getDefinition(inputSearch);
+// });
 
-async function getDefinition(inputSearch) {
+async function getDefinition(word) {
 
-  let response = await fetch(`http://localhost:3000/definition/${inputSearch.value}`,)
+  const response = await fetch(`http://localhost:3000/definition/${word.value}`);
 
-  let data = await response.json();
+  const data = await response.json();
   console.log("this is data", data)
 
- /*  const meaning = document.querySelector("#meaning");
-  //meaning.textContent = data.entry;
+  const obj = await JSON.parse(data.payload)
+  console.log("this is obj", obj)
 
-  const noun = document.querySelector("#noun");
-  //noun.textContent = data.meaning.noun;
+  const meaning = document.querySelector("#meaning");
+  meaning.textContent = obj.entry;
+
+   const noun = document.querySelector("#noun");
+  noun.textContent = obj.meaning.noun;
 
   const verb = document.querySelector("#verb");
-  //verb.textContent = data.meaning.verb;
+  verb.textContent = obj.meaning.verb;
 
   const adverb = document.querySelector("#adverb");
-  //adverb.textContent = data.meaning.adverb;
+  adverb.textContent = obj.meaning.adverb;
 
-  const adjective = document.querySelector("#adjective");
-  //adjective.textContent = data.meaning.adjective; */
+  const adjective = document.querySelector("#adjective"); 
+  adjective.textContent = obj.meaning.adjective;
 
   //inputSearch.value = ""; 
 } 
 
+button.addEventListener("click", () => {
+console.log(inputSearch.value)
+  let meaning = getDefinition(inputSearch)
+  console.log(meaning)
+  
+ inputSearch.value="";
+});
